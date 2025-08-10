26dcf26f5bda1e43cd11e61d7ade5ff22139caf2
mateovelilla <teovelilla@gmail.com>
2025-08-10 17:20:17 -0500

    chore: finished first approach to scrapping one piece fandom

--- scrapper/src/example.spec.ts
+++ scrapper/src/example.spec.ts
@@ -1,58 +1,65 @@
 import { test, expect, chromium } from '@playwright/test';
-const expected_columns = [ 
-  've',
-  'Name',
-  'Chapter',
-  'Episode',
-  'Year',
-  'Note'
+let expected_columns = [
+  { label: 'Name', index: 0, type: 'link'},
+  { label: 'Year', index: 0, type: null},
+  { label: 'Note', index: 0, type: null},
 ]
+const BASE_URL = "https://onepiece.fandom.com"
+const LIST_URL = BASE_URL + '/wiki/List_of_Canon_Characters'
+let characters:any[] = []
 let browser:any, page:any;
 test.beforeAll(async ({}, testInfo) => {
-  testInfo.setTimeout(60000)
+  testInfo.setTimeout(80000)
   browser = await chromium.launch({
     executablePath: "C:\\Program Files\\BraveSoftware\\Brave-Browser\\Application\\brave.exe"
   })
   const context = await browser.newContext();
   page = await context.newPage()
-  await page.goto('https://onepiece.fandom.com/wiki/List_of_Canon_Characters');
+  await page.goto(LIST_URL);
 });
 test('has title', async () => {
   await expect(page).toHaveTitle(/List of Canon Characters/);
 });
 test('has table with columns', async() => {
   const locator = page.locator('.fandom-table:nth-of-type(1) thead th')
-  const columns = locator.map(header => header.textContent.trim() )
-  console.log(columns)
-  // const columns = await page.$$eval('.fandom-table:nth-of-type(1) thead th', ths =>
-  //   ths.map(th => th.textContent.trim())
-  // );
-  // for (const col of expected_columns) {
-  //   expect(columns).toContain(col);
-  // }
+  const columns = await locator.evaluateAll(elements => {
+    return elements.map(header => header.textContent.trim())
+  })
+  for (const col of expected_columns) {
+    expect(columns).toContain(col.label);
+  }
 })
-// test('extract columns', async() => {
-//   const headers = await page.$$eval(
-//     '.fandom-table:nth-of-type(1) thead th',
-//     ths => ths.map(th => th.textContent.trim())
-//   );
-//   // for (let i = 0; i < expected_columns.length; i++) {
-//     // const element = expected_columns[i];
-//     const element = expected_columns[1];
-//     const indexHeader = headers.indexOf(element);
-//     const datosNombre = await page.$$eval(
-//       `.fandom-table:nth-of-type(1) tbody tr td:nth-child(${indexHeader + 1})`,
-//       tds => tds.map(td => ({name: td.textContent.trim(), link: td.child('a')}))
-//     );
-//     console.log('Datos de la columna Nombre:', datosNombre);
-//   // }
-// })
-// test('get started link', async ({ page }) => {
-//   await page.goto('https://playwright.dev/');
-
-//   // Click the get started link.
-//   await page.getByRole('link', { name: 'Get started' }).click();
-
-//   // Expects page to have a heading with the name of Installation.
-//   await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
-// });
+test('extract columns', async({},testInfo) => {
+  testInfo.setTimeout(80000)
+  const locator = page.locator('.fandom-table:nth-of-type(1) tbody tr:nth-child(-n+40)')
+  const header = page.locator('.fandom-table:nth-of-type(1) thead th')
+  const headers = await header.evaluateAll(elements => {
+    return elements
+      .map((header, index) => ({
+        label: header.textContent.trim(),
+        index
+      }))
+  })
+  expected_columns = expected_columns.map(({label, type})=>({
+    label,
+    index: headers.find(col=> col.label === label ).index,
+    type
+  }))
+  for (const element of await locator.all()) {
+    try {
+      let character = {}
+      for (let i = 0; i < expected_columns.length; i++) {
+        const header = expected_columns[i];
+        character[header.label] = await element.locator(`td:nth-child(${header.index + 1})`).textContent()
+        if(header.type === 'link') {
+          const url = await element.locator(`td:nth-child(${header.index + 1}) > a`).getAttribute('href')
+          character['link'] = BASE_URL + url
+        } 
+      }
+      characters.push(character)
+    } catch (error) {
+      console.log(element)
+      console.log(error)
+    }
+  }
+})
