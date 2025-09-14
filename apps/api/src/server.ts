ae3c0e6a7f38968145398dc440330f34135bc841
mateovelilla <teovelilla@gmail.com>
2025-09-13 21:30:33 -0500

    chore: applied good practices part 1

--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -8,6 +8,18 @@ exports.buildServer = async () => {
 	const apollo = new ApolloServer({
 		typeDefs,
 		resolvers,
+		introspection: process.env.NODE_ENV !== 'production',
+		formatError: (formattedError, error) => { // Obfuscating error details
+			console.error('GraphQL Error:', {
+				message: error.message,
+				path: error.path,
+				stack: error.stack,
+			});
+			return {
+			message: 'Internal server error',
+			extensions: { code: 'INTERNAL_SERVER_ERROR' },
+			};
+ 	 	},
 	});
 	await apollo.start();
 	await fastify.register(fastifyApollo(apollo));
